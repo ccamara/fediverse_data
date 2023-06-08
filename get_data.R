8f9c4a4d51371475c809ba7da7efc043cd0e4144
Carlos Cámara <ccamara@pm.me>
2023-06-08 23:01:38 +0100

    First commit

---
+++ get_data.R
@@ -0,0 +1,22 @@
+library(httr)
+library(jsonlite)
+
+
+# FediDB ------------------------------------------------------------------
+
+# FediDB uses a crawler to get info from known servers. The process is somewhat
+# documented here: https://fedidb.org/crawler.html
+# API Documentation: https://fedidb.org/docs/api/v1
+
+res_software <- GET("https://api.fedidb.org/v1/software?limit=40")
+res_servers <- GET("https://api.fedidb.org/v1/servers?limit=40")
+
+df_software <- as.data.frame(fromJSON(rawToChar(res_software$content)))
+df_software$date_scraped <- Sys.Date()
+
+df_servers <- as.data.frame(fromJSON(rawToChar(res_servers$content))$data)
+df_servers$date_scraped <- Sys.Date()
+
+write.csv(df_software, "data/raw/software.csv", row.names = FALSE)
+write.csv(df_servers, "data/raw/servers.csv", row.names = FALSE)
+
